3798a411ad7fae771609852f0e41bbfeb1767bba
Jay Whang <dev@jaywhang.com>
2013-10-05 22:53:26 -0700

    initial import of fingertip tracking

---
+++ game.js
@@ -0,0 +1,14 @@
+var fingertipArray;
+const WIDTH = 300, HEIGHT = 300;
+
+/******* CALLBACK FUNCTIONS *******/
+// Callback to update the fingertip positions
+// Called by leap.js
+function updatePoints(key, points) {
+}
+
+// Callback to highlight a cell
+// Called by leap.js
+function updateKey(key, x, y) {
+}
+
